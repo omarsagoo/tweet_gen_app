beb268c213bc46e450838f72f7c548b97fcd794a
omarsagoo <om.sagoo@gmail.com>
2019-12-01 16:32:19 -0800

    fix changes

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask
-import histogram
+from static.markov import MarkovDict, get_file_clean
 
 app = Flask(__name__)
 
-fish_text = ['one', 'fish', 'two', 'fish', 'red', 'fish', 'blue', 'fish']
-fishtogram = histogram.create_histogram(fish_text)
+file = 'static/diogenes.txt'
+list_file = get_file_clean(file)
+mv = MarkovDict(list_file)
 
 @app.route('/')
 def sample_dict():
-    sample_dict = histogram.test_sampling_dict(fishtogram)
-    return sample_dict
+    
+    return mv.markov_sentence(5)
+
+if __name__ == "__main__":
+    print(mv.markov_sentence(10))
